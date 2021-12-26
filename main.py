09ff4b030247b95f45765af638e41f82a827c6d9
venomega <guilleps92@gmail.com>
2021-12-26 16:29:13 -0500

    added cursor movement and mouse buttons support with special label

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import os
 import sys
+import threading
+import time
+import queue
+queue.memory=[]
 
 if sys.platform != 'linux':
     print("Sorry, this code only works on gnu")
@@ -57,16 +61,117 @@ def keyup(string):
 def keydown(string):
     os.popen(f"xdotool keydown {string}")
 
+def cursor(stream, keys):
+    refresh_rate=0.00000001
+
+    def get_position():
+        data = os.popen("xdotool getmouselocation").read().split(" ")
+        x = int(data[0].split(":")[-1])
+        y = int(data[1].split(":")[-1])
+        return [x,y]
+
+    def cursor_up():
+        while "c_cursor_up" in queue.memory:
+            x,y = get_position()
+            os.popen(f"xdotool mousemove {x} {y - 15}")
+            time.sleep(refresh_rate)
+
+    def cursor_down():
+        while "c_cursor_down" in queue.memory:
+            x,y = get_position()
+            os.popen(f"xdotool mousemove {x} {y + 15}")
+            time.sleep(refresh_rate)
+
+    def cursor_left():
+        while "c_cursor_left" in queue.memory:
+            x,y = get_position()
+            os.popen(f"xdotool mousemove {x - 15} {y}")
+            time.sleep(refresh_rate)
+
+    def cursor_right():
+        while "c_cursor_right" in queue.memory:
+            x,y = get_position()
+            os.popen(f"xdotool mousemove {x + 15} {y}")
+            time.sleep(refresh_rate)
+
+    def kill(name):
+        if name in queue.memory:
+            queue.memory.remove(name)
+
+    if "cursor_up" in keys[-1]:
+        if stream[-4:].hex() == keys[0]:
+            queue.memory.append(keys[-1])
+            threading.Thread(target=cursor_up, name=keys[-1]).start()
+        if stream[-4:].hex() == keys[1]:
+            kill(keys[-1])
+    if "cursor_down" in keys[-1]:
+        if stream[-4:].hex() == keys[0]:
+            queue.memory.append(keys[-1])
+            threading.Thread(target=cursor_down, name=keys[-1]).start()
+        if stream[-4:].hex() == keys[1]:
+            kill(keys[-1])
+    if "cursor_left" in keys[-1]:
+        if stream[-4:].hex() == keys[0]:
+            queue.memory.append(keys[-1])
+            threading.Thread(target=cursor_left, name=keys[-1]).start()
+        if stream[-4:].hex() == keys[1]:
+            kill(keys[-1])
+    if "cursor_right" in keys[-1]:
+        if stream[-4:].hex() == keys[0]:
+            queue.memory.append(keys[-1])
+            threading.Thread(target=cursor_right, name=keys[-1]).start()
+        if stream[-4:].hex() == keys[1]:
+            kill(keys[-1])
+def mouse(stream, keys):
+    def key_down(num):
+        os.popen(f"xdotool keydown click {num}")
+
+    def key_up(num):
+        os.popen(f"xdotool keydown click {num}")
+
+    if "button_1" in keys[-1]:
+        if stream[-4:].hex() == keys[0]:
+            key_down("1")
+        if stream[-4:].hex() == keys[1]:
+            key_up("1")
+    if "button_2" in keys[-1]:
+        if stream[-4:].hex() == keys[0]:
+            key_down("2")
+        if stream[-4:].hex() == keys[1]:
+            key_up("2")
+    if "button_3" in keys[-1]:
+        if stream[-4:].hex() == keys[0]:
+            key_down("3")
+        if stream[-4:].hex() == keys[1]:
+            key_up("3")
+    if "button_4" in keys[-1]:
+        if stream[-4:].hex() == keys[0]:
+            key_down("4")
+        if stream[-4:].hex() == keys[1]:
+            key_up("4")
+    if "button_5" in keys[-1]:
+        if stream[-4:].hex() == keys[0]:
+            key_down("5")
+        if stream[-4:].hex() == keys[1]:
+            key_up("5")
+
+        
+
 def loop(keys, fd):
     while True:
         stream = fd.read(8)
         for i in keys.keys():
-            if stream[-4:].hex() == keys[i][0]:
+            if stream[-4:].hex() == keys[i][0] and not "c_" in keys[i][-1]:
                 print (f"Pressed {i}")
                 keydown(keys[i][2])
-            if stream[-4:].hex() == keys[i][1]:
+            if stream[-4:].hex() == keys[i][1] and not "c_" in keys[i][-1]:
                 print (f"Released {i}")
                 keyup(keys[i][2])
+            if "c_" in keys[i][-1]:
+                if "cursor" in keys[i][-1]:
+                    cursor(stream, keys[i])
+                if "button" in keys[i][-1]:
+                    mouse(stream, keys[i])
 
 def dump_all(fd):
     while True:
